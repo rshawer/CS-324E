99484ac0cba84b219c41371185f252eb5d1e3fad
rshawer <33186139+rshawer@users.noreply.github.com>
2019-02-15 22:16:11 -0600

    Extract all words

--- extract_words.py
+++ extract_words.py
@@ -1,3 +1,5 @@
+import re
+
 def parseString (st):
   s=""
   for i in range (len(st)):
@@ -45,9 +47,50 @@ def word_freq(d):
 
 	file.close()
 
+def processLine(line, wordCount):
+  line = replacePunctuations(line)
+  #words = line.split()
+  for word in line:
+    if word in wordCount:
+      wordCount[word] += 1
+    else:
+      wordCount[word] = 1
+
+def replacePunctuations(line):
+  line = re.findall("[a-z]+", line)
+  '''
+  for ch in line:
+
+  
+    if ch not in "":
+      line = line.replace(ch, " ")
+    '''
+  return line
 
 def main():
     allwords("Sherlock.txt")
-    
+    filename = input("Enter a filename: ").strip()
+    infile = open(filename, "r", encoding='utf8')
+    outfile = open("uniquewords.txt", "w")
+    i = 0
+    wordCount = {}
+    #print(type(wordCount))
+    for line in infile:
+      processLine(line.lower(), wordCount)
+    for word in wordCount:
+      outfile.write(word+"\n")
+      #print(wordCount)
+  #pairs = list(wordCount.keys())
+  #print(pairs)
+  #items = [[x,y] for (y, x) in pairs]
+  #items.sort()
+    '''
+    for i in range(len(items) - 1, len(items) - 11, -1):
+      print(items[i][1])
+      outfile.write(items[i][1] + "\n")
+    '''
+    outfile.close()
+    infile.close()
+    word_freq(wordCount)
 
 main()
